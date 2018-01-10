adcced85c755b5cfa5fc748475fb7204d55f9ac4
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-10 15:54:27 -0700

    Updated the seeds to have 3 users and 10 blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+				 title: "Bloggy #{blog}",
+				 published: false,
+				 topic: "tech",
+				 pic: "http://via.placeholder.com/600x400",
+				 body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+					    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+					    proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				)
+end
+
+puts "10 blogs created"
+
+3.times do |user|
+	User.create!(
+				 email: "asdf#{user}@asdf.com",
+				 password: "asdfasdf",
+				 # last_name: "lasty",
+				 # first_name: "firsty",
+				 # full_name: last_name + first_name
+				)
+end
+
+puts "3 Users created email: asdf[id]@asdf.com, password: asdfasdf"
