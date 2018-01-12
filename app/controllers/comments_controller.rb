4a33f839ba5b890943ca67f79ba14c663290dbb5
Paul Gibbs <pwgibbs10@gmail.com>
2018-01-11 17:03:07 -0700

    Redis gem, fixed errors with databases and models

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,11 @@
 class CommentsController < ApplicationController
-	has_many :comments, dependant: destroy
+	def create
+		@comment = current_user.comments.build(comment_params)
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:content)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,10 @@ class User < ApplicationRecord
   petergate(roles: [:admin, :editor], multiple: false)                                      ##
   ############################################################################################ 
  
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :comments, dependent: :destroy
 end
